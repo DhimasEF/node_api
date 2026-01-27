25d8f6b775151e7f2081e43d4b1d1a049e7fcb35
DhimasEF <dhimasfirmansyah016@gmail.com>
2026-01-27 15:25:00 +0700

    y1

--- config/logDb.js
+++ config/logDb.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql2/promise");
 
 const logDb = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root1234",
+  host: "192.168.6.15",
+  user: "log",
+  password: "log12345",
   database: "logs_sentral",
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
-  port: 3307,
+  port: 3306,
 });
 
 console.log("✅ Log DB Pool Ready");
